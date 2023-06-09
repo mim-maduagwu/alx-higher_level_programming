47af9b4a308a7a8f8ce4aa500cd8fd09f06bb7ce
madMikeLoner <michaelmaduagwu630@gmail.com>
2023-06-08 18:21:12 -0700

    hidden moduls 2

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4 as hd
-    listm = dir(hd)
-    for i in range(len(listm)):
-        if listm[i][0] != "_" and listm[i][1] != "_":
-            print (listm[i])
+    import hidden_4 as hid
+    list = dir(hid)
+    for x in range(len(list)):
+        if(list[x][0] != '_'):
+            print(list[x])
