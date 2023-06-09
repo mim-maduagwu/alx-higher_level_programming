8d811e4a4bdb063085110b7b517670ca6a416d24
madMikeLoner <michaelmaduagwu630@gmail.com>
2023-06-08 18:12:43 -0700

    hidd moduls

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4 as hd
+    listm = dir(hd)
+    for i in range(len(listm)):
+        if listm[i][0] != "_":
+            print (listm[i])
