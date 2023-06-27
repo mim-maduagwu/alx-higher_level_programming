40c2e8af20ef17af9cbad0439bc0d1c94cba5cae
madMikeLoner <michaelmaduagwu630@gmail.com>
2023-06-27 16:33:38 -0700

    python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """
+    creates squar obj
+    """
+    def __init__(self, size):
+        self.__size = size
+    """
+    sets privat attr size
+      __size(int): size of the square
+    """
