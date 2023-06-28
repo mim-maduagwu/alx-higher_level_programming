395b37cbc9d8da3c7506b93a2b9fb70de55201dd
madMikeLoner <michaelmaduagwu630@gmail.com>
2023-06-28 06:38:21 -0700

    python classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""Square module."""
+
+
 class Square:
     """
     creates squar obj
     """
     def __init__(self, size):
+        """
+        sets privat attr size
+        __size(int): size of the square
+        """
         self.__size = size
-    """
-    sets privat attr size
-      __size(int): size of the square
-    """
