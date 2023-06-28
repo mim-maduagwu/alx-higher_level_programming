4478a896953fca9ea04366fab5d287e378b69433
madMikeLoner <michaelmaduagwu630@gmail.com>
2023-06-28 06:43:49 -0700

    python classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,15 +3,19 @@
 
 
 class Square:
+     """Defines a square"""
+
     def __init__(self, size=0):
         """
         creates sqr obj
         """
+
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+        
         """
         initializes instance of a square
         Args:
